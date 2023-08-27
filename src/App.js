3adaae2ef44a08eb191980ae2fad37206077cd5f
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-27 11:03:12 +0530

    class

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const msg = ["Learn React", "Get Job", "Invest your income"];
+const step = 3;
 export default function App() {
   return (
     <div>
@@ -11,12 +13,14 @@ function Hello() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step === 1 ? "active" : ""}>1</div>
+        <div className={step === 2 ? "active" : ""}>2</div>
+        <div className={step === 3 ? "active" : ""}>3</div>
       </div>
 
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step} :{msg[step - 1]}
+      </p>
       <div className="buttons">
         <button style={sid}>Previous</button>
         <button style={sid}>Next</button>
